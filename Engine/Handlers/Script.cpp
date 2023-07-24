2d37c609367201c2c9d34520842e6c7de9ca0c17
JuliusWinther <mattiamcroll@gmail.com>
2023-07-24 16:20:34 +0200

    Update Script.cpp

--- Engine/Handlers/Script.cpp
+++ Engine/Handlers/Script.cpp
@@ -1614,7 +1614,6 @@ int ScriptHandler::popStringBuffer() {
 
 VariableData &ScriptHandler::getVariableData(uint32_t no) {
 	if (no < VARIABLE_RANGE) {
-		printf("%d ", &no);
 		return variable_data[no];
 	}
 
