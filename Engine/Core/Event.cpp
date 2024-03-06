d8839ec869acf4a7a210fdd987d3edc478d9636d
JuliusWinther <mattiamcroll@gmail.com>
2024-03-06 17:33:40 +0100

    Update Event.cpp

--- Engine/Core/Event.cpp
+++ Engine/Core/Event.cpp
@@ -850,8 +850,10 @@ bool ONScripter::keyDownEvent(SDL_KeyboardEvent &event, EventProcessingState &st
 					state.keyState.ctrl |= (event.keysym.scancode == SDL_SCANCODE_LCTRL ? 0x02 : 0x01);
 					internal_slowdown_counter = 0; // maybe a slightly wrong place to do it
 				}
-			if (!skipIsAllowed())
-				break; // Skip not allowed, exit
+			if (!skipIsAllowed()) {
+				gosubReal(event_callback_label, script_h.getCurrent()); // W_TEMP
+				break;                                                  // Skip not allowed, exit
+			}
 			if (last_ctrl_status != state.keyState.ctrl) {
 				skip_effect = true; // allow short-circuiting the current effect with ctrl
 				if (video_skip_mode == VideoSkip::Normal) {
