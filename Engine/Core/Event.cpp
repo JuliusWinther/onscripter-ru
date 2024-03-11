7ca9ba6db83419d6970a953f3836295f23336279
JuliusWinther <mattiamcroll@gmail.com>
2024-03-11 11:48:36 +0100

    test output

--- Engine/Core/Event.cpp
+++ Engine/Core/Event.cpp
@@ -852,9 +852,10 @@ bool ONScripter::keyDownEvent(SDL_KeyboardEvent &event, EventProcessingState &st
 					internal_slowdown_counter = 0; // maybe a slightly wrong place to do it
 				}
 			if (!skipIsAllowed()) {
+				sendToLog(LogLevel::Info, "CTRL DISABLED");
 				// ctrl_pressed_skip_disabled = true; // W_TEMP
-				gosubReal(ctrl_callback_label, script_h.getCurrent(), true); // W_TEMP
-				break;                                                       // Skip not allowed, exit
+				// gosubReal(ctrl_callback_label, script_h.getCurrent(), true); // W_TEMP
+				break; // Skip not allowed, exit
 			}
 			if (last_ctrl_status != state.keyState.ctrl) {
 				skip_effect = true; // allow short-circuiting the current effect with ctrl
