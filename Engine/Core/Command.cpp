712c719ce9b83208be19b6b379a0f63a2c168f36
JuliusWinther <mattiamcroll@gmail.com>
2024-08-18 15:31:09 +0200

    Update Command.cpp

--- Engine/Core/Command.cpp
+++ Engine/Core/Command.cpp
@@ -2231,7 +2231,7 @@ int ONScripter::getscrollposCommand() {
 	AnimationInfo &info = lsp2 ? sprite2_info[no] : sprite_info[no]; // as info
 
 	script_h.readVariable();
-	if (sprite.scrollableInfo.isSpecialScrollable) {
+	if (info.scrollableInfo.isSpecialScrollable) {
 		// AnimationInfo::ScrollableInfo &si = sprite.scrollableInfo;
 		script_h.setInt(&script_h.current_variable, info.scrollable.y);
 	} else {
